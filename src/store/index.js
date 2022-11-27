12b454b80eea0afb4e4103515aee3d76a4dbfa94
MMK-13 <kantemamadou13@gmail.com>
2022-11-27 05:26:30 +0000

    Initial commit

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,13 @@
 import { createStore } from 'vuex'
 
+const state = {}
+const getters = {}
+const mutations = {}
+const actions = {}
+
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+	state: state,
+	getters: getters,
+	mutations: mutations,
+	actions: actions,
 })
